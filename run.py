7a79e45a5ef3f501d2ab4767b4ac20aa8b7fa409
lwanderley <lwanderley@ucsd.edu>
2023-02-07 18:33:31 -0800

    Tue commit

--- run.py
+++ run.py
@@ -3,10 +3,6 @@
 import sys
 import json
 import pandas as pd
-import csv
-from sklearn.model_selection import train_test_split
-
-
 
 sys.path.insert(0, 'src')
 from etl import get_data, save_data
@@ -19,15 +15,16 @@ def main(targets):
 
         data = get_data(data_config)
         save_data(data_config, data) #load and train the model with test_load data
+        # return print("test ok")
         return
 
-    # elif 'etl' in targets:
+    elif 'run' in targets:
 
-    #     return
+        return print("run ok")
 
-    # else:
+    else:
 
-    #     return
+        return print("error target")
 
 if __name__ == '__main__':
 
